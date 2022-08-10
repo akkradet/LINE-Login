046fb1b4af33fb5dd107c7a5c62210b07ab32537
akkradet <akkradet7@gmail.com>
2022-08-10 17:04:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const liff = window.liff;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      userLineID: '',
+      pictureUrl: ''
+    };
+  }
+
+  componentDidMount = async() => {
+    await liff.init({ liffId: `1657377127-wpxJDpGg` }).catch(err=>{throw err});
+    if (liff.isLoggedIn()) {
+      let getProfile = await liff.getProfile();
+      this.setState({
+        name: getProfile.displayName,
+        userLineID: getProfile.userId,
+        pictureUrl: getProfile.pictureUrl,
+      });
+    }else{
+      liff.login();
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="support">
+            <p>ชื่อ {this.state.name}</p>
+            <p>Line ID {this.state.userLineID}</p>
+            <img alt='pic' src={this.state.pictureUrl} />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
